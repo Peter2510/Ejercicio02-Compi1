74bc19d677af716f89a5974475c3d32770d9fc5c
Pedro Gordillo <pedrogordillo2510@gmail.com>
2022-02-14 03:25:22 -0600

    Subiendo proyecto

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jflex.exceptions.SilentExit;
+import ventanas.Inicial;
+
+/**
+ *
+ * @author GORDILLO G
+ */
+public class Principal {
+    public static void main(String[] args)  {
+        
+        
+        Inicial ini = new Inicial();
+        ini.setVisible(true);
+    }
+    
+    
+ 
+}
